8bceaff7df84153cdee312790dbbcc5ff569b2b2
Alexander Antonov <alexandr-post@yandex.ru>
2016-08-07 22:58:54 +0300

    Game was updated

--- app/resources/blocks/game/game.jsx
+++ app/resources/blocks/game/game.jsx
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
 
+import Board from '../board/board';
+
 /**
  * @class
  * @extends Component
+ *
+ * @props [array] [matrix=[[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
  */
 export default class Game extends Component {
     render() {
-        return <div className="game"></div>;
+        return <div className="game">
+            <Board matrix={this.props.matrix}/>
+        </div>;
     }
 }
+
+/**
+ * @static
+ */
+Game.defaultProps = {
+    matrix: [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+};
