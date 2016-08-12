91c621de47012cd0805a8cdbf5448a0ab1771e30
Alexander Antonov <alexandr-post@yandex.ru>
2016-08-12 19:41:15 +0300

    SettingsControl was updated

--- app/resources/blocks/settings/settings__control.jsx
+++ app/resources/blocks/settings/settings__control.jsx
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import {activatePopup} from '../../../actions/';
 
 /**
  * @class
  * @extends Component
  */
-export default class SettingsControl extends Component {
+class SettingsControl extends Component {
+    constructor() {
+        super();
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.props.activatePopup(true);
+    }
+
     render() {
-        return <button className="settings__control" type="button"></button>;
+        return <button className="settings__control" onClick={this.handleClick} type="button"></button>;
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return {
+        activatePopup
+    }
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(SettingsControl);
