2d424b11d233ca8c1d252001689d317ba54aba6a
Alexander Antonov <alexandr-post@yandex.ru>
2016-08-22 02:02:54 +0300

    css for Popup was updated

--- app/resources/blocks/popup/popup.jsx
+++ app/resources/blocks/popup/popup.jsx
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 
+import classnames from 'classnames';
+
 /**
  * @class
  * @extends Component
  */
 export default class Popup extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mounted: false
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            mounted: true
+        });
+    }
+
     render() {
         return (
-            <div className="popup">
+            <div className={classnames('popup', {
+                'popup_mounted': this.state.mounted
+            })}>
                 <div className="popup__box">
                     <h4 className="popup__title">{this.props.title}</h4>
 
