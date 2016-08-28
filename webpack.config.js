87d5e92117946acc6dbf67566463be2807789d9e
Alexander Antonov <alexandr-post@yandex.ru>
2016-08-29 02:18:07 +0300

    webpack.config.js was under refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -4,99 +4,115 @@ const path = require('path');
 const webpack = require('webpack');
 
 const __DEV__ = JSON.stringify(JSON.parse(process.env.DEBUG || 'false')) === 'true';
-const devFlagPlugin = new webpack.DefinePlugin({
-    __DEV__: __DEV__
-});
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-let extractHTML = new ExtractTextPlugin('index.html');
-let extractCSS = new ExtractTextPlugin('[name].css');
+const extractHTML = new ExtractTextPlugin('index.html');
+const extractCSS = new ExtractTextPlugin('[name].css');
 
+const HOST = 'localhost';
+const PORT = 8080;
+
+let config = {};
+
+// Dev Tool
+config.devtool = __DEV__ ? 'eval' : false;
+
+// Watch
+config.watch = __DEV__;
+
+// Entry
+config.entry = (() => {
+    let result = [];
+
+    if (__DEV__) {
+        result = [].concat(result, [
+            `webpack-dev-server/client?http://${HOST}:${PORT}`,
+            'webpack/hot/only-dev-server'
+        ]);
+    }
+    result.push('./app/resources/pages/index');
+
+    return result;
+}());
+
+// Output
+config.output = {
+    filename: '[name].js',
+    path: path.join(__dirname, 'build'),
+    publicPath: '/build/'
+};
+
+// Module
+config.module = {
+    loaders: [
+        {
+            test: /\.css$/,
+            loader: extractCSS.extract('css!postcss')
+        },
+        {
+            test: /\.html$/,
+            loader: extractHTML.extract('raw!html-minify')
+        },
+        {
+            test: /\.(jpg|png|svg)$/,
+            loader: 'file?name=[hash].[ext]'
+        },
+        {
+            test: /\.(js|jsx)$/,
+            loaders: (() => {
+                let result = [];
+
+                if (__DEV__) {
+                    result.push('react-hot');
+                }
+                result.push('babel');
+
+                return result;
+            }()),
+            include: [
+                path.join(__dirname, 'app/'),
+                path.join(__dirname, 'node_modules/matrix-slicer/')
+            ]
+        }
+    ]
+};
+
+// Plugin
+config.plugins = (() => {
+    let result = [
+        new webpack.DefinePlugin({
+            __DEV__: __DEV__
+        }),
+        extractHTML,
+        extractCSS
+    ];
+
+    if (__DEV__) {
+        result = [].concat(result, [
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoErrorsPlugin()
+        ]);
+    } else {
+        result = [].concat(result, [
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.OccurenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                acorn: true
+            })
+        ]);
+    }
+
+    return result;
+}());
+
+// PostCSS
 const autoprefixer = require('autoprefixer');
 const csso = require('postcss-csso');
 const atImport = require('postcss-import');
 const url = require('postcss-url');
 
-const HOST = 'localhost';
-const PORT = 8080;
-
-let entry = [
-    './app/resources/pages/index'
-];
-let plugins = [
-    devFlagPlugin,
-    extractHTML,
-    extractCSS
-];
-let loaders = [
-    {
-        test: /\.css$/,
-        loader: extractCSS.extract('css!postcss')
-    },
-    {
-        test: /\.html$/,
-        loader: extractHTML.extract('raw!html-minify')
-    },
-    {
-        test: /\.(jpg|png|svg)$/,
-        loader: 'file?name=[hash].[ext]'
-    }
-];
-
-if (__DEV__) {
-    entry.push(`webpack-dev-server/client?http://${HOST}:${PORT}`);
-    entry.push('webpack/hot/only-dev-server');
-
-    plugins.push(new webpack.HotModuleReplacementPlugin());
-    plugins.push(new webpack.NoErrorsPlugin());
-
-    loaders.push({
-        test: /\.(js|jsx)$/,
-        loaders: [
-            'react-hot',
-            'babel'
-        ],
-        include: path.join(__dirname, 'app/')
-    });
-} else {
-    plugins.push(new webpack.optimize.DedupePlugin());
-    plugins.push(new webpack.optimize.OccurenceOrderPlugin());
-    plugins.push(new webpack.optimize.UglifyJsPlugin({
-        acorn: true
-    }));
-
-    loaders.push({
-        test: /\.(js|jsx)$/,
-        loaders: [
-            'babel'
-        ],
-        include: [
-            path.join(__dirname, 'app/'),
-            path.join(__dirname, 'node_modules/matrix-slicer/')
-        ]
-    });
-};
-
-module.exports = {
-    devtool: __DEV__ ? 'eval' : false,
-
-    watch: __DEV__,
-
-    entry: entry,
-
-    output: {
-        filename: '[name].js',
-        path: path.join(__dirname, 'build'),
-        publicPath: '/build/'
-    },
-
-    plugins: plugins,
-
-    module: {
-        loaders: loaders
-    },
-
-    postcss: [
+config.postcss = (() => {
+    let result = [
         atImport({
             path: [
                 path.join(__dirname, 'app/resources/blocks/'),
@@ -111,23 +127,32 @@ module.exports = {
             browsers: [
                 'last 2 versions'
             ]
-        }),
-        csso({
+        })
+    ];
+
+    if (__DEV__) {
+        result.push(csso({
             debug: 3,
             restructure: true
-        })
-    ],
-
-    resolve: {
-        extensions: [
-            '',
-            '.js',
-            '.jsx'
-        ]
-    },
-
-    devServer: {
-        host: HOST,
-        port: PORT
+        }));
     }
+
+    return result;
+}());
+
+// Resolve
+config.resolve = {
+    extensions: [
+        '',
+        '.js',
+        '.jsx'
+    ]
 };
+
+// Dev Server
+config.devServer = (__DEV__ ? {
+    host: HOST,
+    port: PORT
+} : null);
+
+module.exports = config;
