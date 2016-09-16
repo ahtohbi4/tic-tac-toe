07ee74a7d11b4a15afe62482031f956b241c4751
Alexander Antonov <alexandr-post@yandex.ru>
2016-09-16 23:55:52 +0300

    .popup_mounted was removed

--- app/resources/blocks/popup/popup.jsx
+++ app/resources/blocks/popup/popup.jsx
@@ -7,25 +7,9 @@ import classnames from 'classnames';
  * @extends Component
  */
 export default class Popup extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            mounted: false
-        };
-    }
-
-    componentDidMount() {
-        this.setState({
-            mounted: true
-        });
-    }
-
     render() {
         return (
-            <div className={classnames('popup', {
-                'popup_mounted': this.state.mounted
-            })}>
+            <div className="popup">
                 <div className="popup__box">
                     <h4 className="popup__title">{this.props.title}</h4>
 
