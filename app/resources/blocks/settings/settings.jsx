c720ef782bd75303c8a9a281cf091683e1b7fde4
Alexander Antonov <alexandr-post@yandex.ru>
2016-09-18 20:29:35 +0300

    method onSettingsUpdate for Settings was created

--- app/resources/blocks/settings/settings.jsx
+++ app/resources/blocks/settings/settings.jsx
@@ -17,7 +17,12 @@ class SettingsBlank extends Component {
     constructor() {
         super();
 
+        this.state = {
+            wasChanged: false
+        };
+
         this.handleStartNewGame = this.handleStartNewGame.bind(this);
+        this.onSettingsUpdate = this.onSettingsUpdate.bind(this);
         this.handleResume = this.handleResume.bind(this);
         this.handleApply = this.handleApply.bind(this);
     }
@@ -55,6 +60,26 @@ class SettingsBlank extends Component {
         this.props.activatePopup(false);
     }
 
+    onSettingsUpdate() {
+        if (
+            this._inputWidth.state.value === this.props.game.matrix[0].length &&
+            this._inputHeight.state.value === this.props.game.matrix.length &&
+            this._inputVictoryChainsLength.state.value === this.props.game.victoryChainsLength
+        ) {
+            if (this.state.wasChanged) {
+                this.setState({
+                    wasChanged: false
+                });
+            }
+        } else {
+            if (!this.state.wasChanged) {
+                this.setState({
+                    wasChanged: true
+                });
+            }
+        }
+    }
+
     render() {
         const width = this.props.game.matrix[0].length;
         const height = this.props.game.matrix.length;
@@ -69,23 +94,43 @@ class SettingsBlank extends Component {
 
                     <div>
                         <label>Width:</label>
-                        <InputNumber value={width} minValue={3} maxValue={10} ref={c => this._inputWidth = c}/>
+                        <InputNumber
+                            value={width}
+                            minValue={3}
+                            maxValue={10}
+                            ref={c => this._inputWidth = c}
+                            onChange={this.onSettingsUpdate}/>
                     </div>
 
                     <div>
                         <label>Height:</label>
-                        <InputNumber value={height} minValue={3} maxValue={10} ref={c => this._inputHeight = c}/>
+                        <InputNumber
+                            value={height}
+                            minValue={3}
+                            maxValue={10}
+                            ref={c => this._inputHeight = c}
+                            onChange={this.onSettingsUpdate}/>
                     </div>
 
                     <div>
                         <label>Length of the wins Chain:</label>
-                        <InputNumber value={victoryChainsLength} minValue={3} maxValue={10} ref={c => this._inputVictoryChainsLength = c}/>
+                        <InputNumber
+                            value={victoryChainsLength}
+                            minValue={3}
+                            maxValue={10}
+                            ref={c => this._inputVictoryChainsLength = c}
+                            onChange={this.onSettingsUpdate}/>
                     </div>
 
                     <div>
-                        <button onClick={this.handleResume} type="reset">Resume</button>
+                        <button
+                            onClick={this.handleResume}
+                            type="reset">Resume</button>
 
-                        <button onClick={this.handleApply} type="submit">Apply</button>
+                        <button
+                            onClick={this.handleApply}
+                            disabled={!this.state.wasChanged}
+                            type="submit">Apply</button>
                     </div>
                 </form>
             </Popup>
