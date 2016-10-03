b185844bb005a9ec0946ec9f252a0f4111f21662
Alexander Antonov <alexandr-post@yandex.ru>
2016-10-04 00:07:44 +0300

    Button was created

---
+++ app/resources/blocks/button/button.jsx
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+
+import classnames from 'classnames';
+
+import './button.css';
+
+/**
+ * @class
+ * @extends Component
+ */
+export default class Button extends Component {
+    /**
+     * @static
+     */
+    static propTypes = {
+        className: React.PropTypes.string,
+        disabled: React.PropTypes.bool,
+        name: React.PropTypes.string,
+        type: React.PropTypes.oneOf[
+            'button',
+            'reset',
+            'submit'
+        ]
+    }
+
+    /**
+     * @static
+     */
+    static defaultProps = {
+        className: null,
+        disabled: false,
+        name: null,
+        type: 'button'
+    }
+
+    render() {
+        return (
+            <button
+                className={classnames(
+                    'button',
+                    this.props.className
+                )}
+                disabled={this.props.disabled}
+                name={this}
+                type={this.props.type}>
+                {this.props.children}
+            </button>
+        );
+    }
+}
