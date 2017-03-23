b53cb6b49c7566700d7fd90d2c3354ed93fb0411
Alexander Antonov <alexandr-post@yandex.ru>
2017-03-23 22:10:42 +0300

    redux-devtools was replaced with redux-devtools-extension

--- app/resources/pages/index.jsx
+++ app/resources/pages/index.jsx
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import DevTools from '../../utils/DevTools';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import reducers from '../../reducers';
-const store = createStore(reducers, DevTools.instrument());
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 import App from '../blocks/app/app';
 
@@ -19,16 +21,13 @@ class Root extends Component {
     render() {
         return (
             <Provider store={store}>
-                <div>
-                    <App/>
-                    {__DEV__ ? <DevTools/> : null}
-                </div>
+                <App />
             </Provider>
         );
     }
 }
 
 render(
-    <Root/>,
+    <Root />,
     document.getElementById('root')
 );
